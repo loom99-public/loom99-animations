2f27bd1e5475d8359364549f1771b4031faf2da4
Brandon Fryslie <530235+brandon-fryslie@users.noreply.github.com>
2025-12-13 03:38:14 -0700

    Deploy github pages

--- gallery/vite.config.ts
+++ gallery/vite.config.ts
@@ -4,6 +4,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/loom99-animations/' : '/',
   plugins: [
     react(),
     viteStaticCopy({
